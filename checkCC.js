19f34baacf08186da49e0b6f79ec542e7f67ca9d
Walter Chavarria <wchavarria03@gmail.com>
2018-04-23 15:14:20 -0600

    Refactoring code v2

--- checkCC.js
+++ checkCC.js
@@ -20,119 +20,116 @@
  *          7         JCB
  **/
 
-module.exports = function checkCC() {
-  return 'Working';
-}
-// module.exports = function checkCC(creditCard) {
-//   const ccTypes = {
-//     "-1": { code: -1, label: "invalid"},
-//     1: {code: 1, label: "master"},
-//     2: {code: 2, label: "visa"},
-//     3: {code: 3, label: "american_express"},
-//     4: {code: 4, label: "invalid"},
-//     5: {code: 5, label: "discover"},
-//     6: {code: 6, label: "invalid"},
-//     7: {code: 7, label: "invalid"},
-//   };
+module.exports = function checkCC(creditCard) {
+  const ccTypes = {
+    "-1": { code: -1, label: "invalid"},
+    1: {code: 1, label: "master"},
+    2: {code: 2, label: "visa"},
+    3: {code: 3, label: "american_express"},
+    4: {code: 4, label: "invalid"},
+    5: {code: 5, label: "discover"},
+    6: {code: 6, label: "invalid"},
+    7: {code: 7, label: "invalid"},
+  };
 
-//   function __returnResult (isValid, code) {
-//     var res = ccTypes["" + code];
-//     res.isValid = isValid;
-//     return res;
-//   }
-//   function __invalidResponse() {
-//     var code = -1; //invalid
-//     var isValid = -1; //invalid
-//     return __returnResult(isValid, code);
-//   };
+  function __returnResult (isValid, code) {
+    var res = ccTypes["" + code];
+    res.isValid = isValid;
+    return res;
+  }
+  function __invalidResponse() {
+    var code = -1; //invalid
+    var isValid = -1; //invalid
+    return __returnResult(isValid, code);
+  };
 
-//   function __descomposeIntoArray(creditCard) {
-//     for (var creditCardArray = [], index = 0, tempCC = creditCard; 0 !== tempCC; index++) {
-//       creditCardArray[index] = tempCC % 10;
-//       tempCC -= creditCardArray[index];
-//       tempCC /= 10;
-//     }
-//     return creditCardArray;
-//   };
+  function __descomposeIntoArray(creditCard) {
+    for (var creditCardArray = [], index = 0, tempCC = creditCard; 0 !== tempCC; index++) {
+      creditCardArray[index] = tempCC % 10;
+      tempCC -= creditCardArray[index];
+      tempCC /= 10;
+    }
+    return creditCardArray;
+  };
 
-//   function __addStartWithProtoEvent(text, string) {
-//     String.prototype.startsWith = function (string) {
-//       return this.match("^" + string) == string;
-//     };
-//   };
+  function __addStartWithProtoEvent(text, string) {
+    String.prototype.startsWith = function (string) {
+      return this.match("^" + string) == string;
+    };
+  };
 
-//   function __cleanCCParameter(creditCard) {
-//     return creditCard.replace(/[^0-9]/g, ""); 
-//   }
+  function __cleanCCParameter(creditCard) {
+    return creditCard.replace(/[^0-9]/g, ""); 
+  }
 
-//   creditCard.checkCC = function (creditCard) {
-//     __addStartWithProtoEvent();
-//     creditCard = __cleanCCParameter(creditCard);
-//     var creditCardArray = __descomposeIntoArray(creditCard);
-//     var creditCardLength = creditCard.length;
+  creditCard.checkCC = function (creditCard) {
+    __addStartWithProtoEvent();
+    creditCard = __cleanCCParameter(creditCard);
+    var creditCardArray = __descomposeIntoArray(creditCard);
+    var creditCardLength = creditCard.length;
     
-//     if (creditCardLength < 13) {
-//       return __invalidResponse();
-//     }
+    if (creditCardLength < 13) {
+      return __invalidResponse();
+    }
     
-//     var isValid = -1;
-//     var code = -1;
+    var isValid = -1;
+    var code = -1;
 
-//     if (creditCard.startsWith("5")) {
-//       if (creditCardLength === 16) {
-//         isValid = 1;
-//       }
-//       code = 1;
-//     } else if (creditCard.startsWith("4")) { 
-//       if (creditCardLength === 16 || creditCardLength === 13) {
-//         isValid = 1;
-//       }
-//       code = 2;
-//     } else if (creditCard.startsWith("34") || creditCard.startsWith("37")) { 
-//       if (creditCardLength === 15) {
-//         isValid = 1;
-//       }
-//       code = 3;
-//     } else if (creditCard.startsWith("36")
-//       || creditCard.startsWith("38")
-//       || creditCard.startsWith("300")
-//       || creditCard.startsWith("301")
-//       || creditCard.startsWith("302")
-//       || creditCard.startsWith("303")
-//       || creditCard.startsWith("304")
-//       || creditCard.startsWith("305")) { 
-//         if (creditCardLength === 14) {
-//           isValid = 1;
-//         }
-//       code = 4;
-//     } else if (creditCard.startsWith("6011")) {
-//       if (creditCardLength === 15 || creditCardLength === 16) {
-//         isValid = 1;
-//       }
-//       code = 5;
-//     } else if (creditCard.startsWith("2014") || creditCard.startsWith("2149")) {
-//       if (creditCardLength === 15 || creditCardLength === 16) {
-//         isValid = 1;
-//       }
-//       code = 6;
-//     } else {
-//       if ((creditCard.startsWith("3") && 16 === creditCardLength) 
-//       || (creditCard.startsWith("2131") || creditCard.startsWith("1800")) 
-//       || (creditCardLength === 15) ) {
-//         isValid = 1;
-//       }
-//       code = 7;
-//     }
+    if (creditCard.startsWith("5")) {
+      if (creditCardLength === 16) {
+        isValid = 1;
+      }
+      code = 1;
+    } else if (creditCard.startsWith("4")) { 
+      if (creditCardLength === 16 || creditCardLength === 13) {
+        isValid = 1;
+      }
+      code = 2;
+    } else if (creditCard.startsWith("34") || creditCard.startsWith("37")) { 
+      if (creditCardLength === 15) {
+        isValid = 1;
+      }
+      code = 3;
+    } else if (creditCard.startsWith("36")
+      || creditCard.startsWith("38")
+      || creditCard.startsWith("300")
+      || creditCard.startsWith("301")
+      || creditCard.startsWith("302")
+      || creditCard.startsWith("303")
+      || creditCard.startsWith("304")
+      || creditCard.startsWith("305")) { 
+        if (creditCardLength === 14) {
+          isValid = 1;
+        }
+      code = 4;
+    } else if (creditCard.startsWith("6011")) {
+      if (creditCardLength === 15 || creditCardLength === 16) {
+        isValid = 1;
+      }
+      code = 5;
+    } else if (creditCard.startsWith("2014") || creditCard.startsWith("2149")) {
+      if (creditCardLength === 15 || creditCardLength === 16) {
+        isValid = 1;
+      }
+      code = 6;
+    } else {
+      if ((creditCard.startsWith("3") && 16 === creditCardLength) 
+      || (creditCard.startsWith("2131") || creditCard.startsWith("1800")) 
+      || (creditCardLength === 15) ) {
+        isValid = 1;
+      }
+      code = 7;
+    }
 
-//     var h, g = 0;
-//     for (h = 1; creditCardLength > h; h += 2) {
-//       var i = 2 * creditCardArray[h];
-//       g += i % 10, g += (i - i % 10) / 10
-//     }
-//     for (h = 0; creditCardLength > h; h += 2) {
-//       g += creditCardArray[h];
-//     }
-//     isValid = 0 !== g % 10 ? -1 : isValid;
-//     return __returnResult(isValid, code);
-//   }
-// }(this);
+    var h, g = 0;
+    for (h = 1; creditCardLength > h; h += 2) {
+      var i = 2 * creditCardArray[h];
+      g += i % 10, g += (i - i % 10) / 10
+    }
+    for (h = 0; creditCardLength > h; h += 2) {
+      g += creditCardArray[h];
+    }
+    isValid = 0 !== g % 10 ? -1 : isValid;
+    return __returnResult(isValid, code);
+  }
+}(this);
